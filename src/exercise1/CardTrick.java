cad3ad43dea32746c486af3a96cc9d504857f969
kampofo6 <ampofoko@sheridancollege.ca>
2023-02-03 20:12:28 -0500

    Added a Card Hand algorithm that takes 7 random cards in a deck, prompting the user to guess the card and then displaying each card.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,30 +1,68 @@
 package exercise1;
-
+import java.util.Scanner;
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
+ * A class   and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * 
+ * @author Kojo Ampofo Feburary 3rd 2023
  */
 public class CardTrick {
     
     public static void main(String[] args) {
         
-        Card[] hand = new Card[7];
+        Scanner scan = new Scanner(System.in);
+        
+         Card [] hand = new Card[7];
 
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
+            Card mycard = new Card();
+            hand[i] = mycard;
             //card.setValue(insert call to random number generator here)
-            // 
+              mycard.setValue((int)(Math.random() * 13 )+1);
+                    
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            mycard.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
         }
+        System.out.print(hand);
 
         // insert code to ask the user for Card value and suit, create their card
+        Card card2 = new Card();
+        
+        System.out.println("Enter your card value: ");
+        int value = scan.nextInt();
+        System.out.println("Enter your card suit: ");
+        String Suit = scan.next();
+        
+        card2.setValue(value);
+        card2.setSuit(Suit);
+        
         // and search the hand here. 
+        boolean checker = false; 
+        for (int i = 0; i < hand.length; i++ ){
+            System.out.println(hand[i].getValue());
+             System.out.println(hand[i].getSuit());
+             
+             if( hand[i].getSuit() == Suit && hand[i].getValue() == value){
+             
+                 checker = true;
+             }
+           
+        }
+          
+        if(checker==true){
+        System.out.println("Your card is in the deck");
+
+        }
+             
+        else{
+            System.out.println("Your card is not in the deck");
+        }
+         
+        
         // Hint: You can ask for values 1 to 10, and then
         //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
@@ -33,35 +71,11 @@ public class CardTrick {
         
         // If the guess is successful, invoke the printInfo() method below.
         
-    }
-
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
-    private static void printInfo() {
     
-        System.out.println("Congratulations, you guessed right!");
-        System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
-        System.out.println();
-        
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
-        System.out.println();
-        
-    
-    }
-
 }
+
+    
+    
+    }
+
+
